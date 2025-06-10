0a8581d6b3d6a51d3dea0d951d8f42409e58727c
onome-git <reghaonome@gmail.com>
2025-06-10 15:07:22 -0400

    Create bucket.tf

---
+++ bucket.tf
@@ -0,0 +1,12 @@
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-modern-secure-bucket-123456789"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
