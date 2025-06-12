91725acce4ff4cbb106c626b937ee4da3290cc46
onome-git <reghaonome@gmail.com>
2025-06-12 15:08:21 -0400

    Update variable.tf

--- modules/ecs_cluster/variable.tf
+++ modules/ecs_cluster/variable.tf
@@ -1,1 +1,39 @@
-dfgfhgjh
+variable "cluster_names" {
+  type = map(string)
+  description = "Map of cluster names."
+}
+
+variable "container_image" {
+  type        = string
+  description = "Docker image for container."
+  default     = "mongo"
+}
+
+variable "container_port" {
+  type        = number
+  description = "Port the container exposes."
+  default     = 8080
+}
+
+variable "cpu" {
+  type        = string
+  description = "CPU units for the task."
+  default     = "256"
+}
+
+variable "memory" {
+  type        = string
+  description = "Memory for the task."
+  default     = "512"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID for the load balancer."
+}
+
+variable "desired_count" {
+  type        = number
+  description = "Number of tasks to run."
+  default     = 3
+}
