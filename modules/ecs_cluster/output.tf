baace362dc2cc0c82bea075d14bd6eb0314d7911
onome-git <reghaonome@gmail.com>
2025-06-12 15:08:53 -0400

    Update output.tf

--- modules/ecs_cluster/output.tf
+++ modules/ecs_cluster/output.tf
@@ -1,1 +1,10 @@
-ssfdgfhg
+output "cluster_ids" {
+  value = { for k, c in aws_ecs_cluster.clusters : k => c.id }
+}
+
+output "service_arns" {
+  value = tomap({
+    for name, svc in aws_ecs_service.services :
+    name => lookup(svc, "arn", "no-arn-found")
+  })
+}
