2f8f6e15f9b02b07a95486cf268da8fa61fbac47
onome-git <reghaonome@gmail.com>
2025-06-12 15:12:56 -0400

    Update variable.tf

--- modules/ec2_instance/variable.tf
+++ modules/ec2_instance/variable.tf
@@ -1,1 +1,14 @@
-dsfgh
+variable "key_name" {
+  description = "Name of the EC2 key pair"
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs to assign"
+  type        = list(string)
+}
+
+variable "top_movies" {
+  description = "List of movie names to use for EC2 instance names"
+  type        = list(string)
+}
