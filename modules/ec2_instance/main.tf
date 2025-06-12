cc476279a67b2f0290fdd9341842410712fc5a37
onome-git <reghaonome@gmail.com>
2025-06-12 15:12:26 -0400

    Update main.tf

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,1 +1,12 @@
-fklghk
+resource "aws_instance" "ec2_movies" {
+  for_each = toset(var.top_movies)
+
+  ami                    = "ami-0c7217cdde317cfec"
+  instance_type          = "t2.micro"
+  key_name               = var.key_name                
+  vpc_security_group_ids = var.security_group_ids      
+
+  tags = {
+    Name = each.key
+  }
+}
