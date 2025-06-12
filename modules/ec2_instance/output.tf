bbf6625e97d76c0d23b39dfb227dc8cbf6fd8032
onome-git <reghaonome@gmail.com>
2025-06-12 15:13:15 -0400

    Update output.tf

--- modules/ec2_instance/output.tf
+++ modules/ec2_instance/output.tf
@@ -1,1 +1,15 @@
-dfg
+output "ec2_public_ips" {
+  description = "Public IPs of EC2 instances"
+  value = {
+    for name, instance in aws_instance.ec2_movies :
+    name => instance.public_ip
+  }
+}
+
+output "ec2_instance_ids" {
+  description = "Instance IDs of EC2 instances"
+  value = {
+    for name, instance in aws_instance.ec2_movies :
+    name => instance.id
+  }
+}
